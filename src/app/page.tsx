fb0c65c80a7e4f803475134d936b8080118feae0
ShivamBunge <shivambunge9898@gmail.com>
2023-07-05 19:17:00 +0530

    fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export const metadata = {
 export default function HomePage() {
   return (
     <>
-      <div className="flex relative">
+      <div className="flex relative items-center">
         {/* <div className="absolute top-0 left-0 w-full h-full">
           <div className="h-20 w-20 bg-customSecondary rounded-full"></div>
           <div className="h-10 w-10 bg-customSecondary rounded-full absolute bottom-0 left-20"></div>
@@ -17,20 +17,29 @@ export default function HomePage() {
           <div className="h-10 w-10 bg-customSecondary rounded-full absolute "></div>
 
         </div> */}
-        <div className="relative mx-auto h-80 w-80 ">
+        {/* <div className="relative mx-auto h-[20rem] md:h-[28rem] w-80 ">
           <Image
-            className="rounded-t-full opacity-90"
+            className="rounded-t-full opacity-80"
+            src={img}
+            alt="cover image"
+            fill
+            style={{ objectFit: 'cover' }}
+          />
+        </div> */}
+        <div className="relative mx-auto h-[20rem] sm:h-[30rem] md:h-[40rem] w-screen ">
+          <Image
+            className="opacity-80"
             src={img}
             alt="cover image"
             fill
             style={{ objectFit: 'cover' }}
           />
         </div>
-        <div className="absolute inset-0 flex flex-col justify-start mt-12 ">
+        <div className="absolute flex flex-col sm:left-10">
           <h1 className="text-4xl md:text-6xl">
             Subconsciously
           </h1>
-          <h3 className="text-xl md:text-2xl mt-5">
+          <h3 className="text-xl md:text-2xl mt-5 text-secondary">
             Exploring the depths of the mind
           </h3>
         </div>
@@ -43,30 +52,30 @@ export default function HomePage() {
         <h1 className="text-4xl font-semibold text-center">
           About
         </h1>
-        <p className="mt-9 text-xl font-thin">
+        <p className="mt-9 text-lg font-thin">
           Join us on our journey of enlightenment and exploration as we delve into health and wellness. We aim to educate everyone about health covering topics about mental and physical health, fitness. Mission is to inspire and motivate you to discover new perspectives and lead a fulfilling life. Self empowerment is the key to unlocking our full potential and to serve everyone around us.
         </p>
-        <p className="mt-9 text-xl font-thin">
+        <p className="mt-9 text-lg font-thin">
           Get ready to be captivated by thought-provoking content and subscribe to our channel if this truly excites you and resonates with your passion.
         </p>
       </div>
 
       <div className="mt-36 flex flex-wrap md:flex-nowrap gap-10 ">
-        <div className="bg-customSecondary p-5 rounded-lg">
+        <div className="bg-customSecondary p-8 rounded-lg">
           <h1 className="text-3xl md:text-5xl my-8">
             Subconsciously
           </h1>
-          <p className="text-xl">
+          <p className="text-lg">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta vitae possimus delectus hic quidem ad magnam quae similique commodi sed. Laudantium maxime totam, ex dolorem facere pariatur saepe beatae excepturi.
 
           </p>
 
         </div>
         <div className="flex flex-col gap-10 ">
-          <div className="bg-customSecondary  p-5 text-xl rounded-lg">
+          <div className="bg-customSecondary  p-8 text-lg rounded-lg">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta vitae possimus delectus hic quidem ad magnam quae similique commodi sed. Laudantium maxime totam, ex dolorem facere pariatur saepe beatae excepturi.
           </div>
-          <div className="bg-customSecondary p-5 text-xl rounded-lg">
+          <div className="bg-customSecondary p-5 text-lg rounded-lg">
             Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta vitae possimus delectus hic quidem ad magnam quae similique commodi sed. Laudantium maxime totam, ex dolorem facere pariatur saepe beatae excepturi.
 
           </div>
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,8 +1,9 @@
 import Link from 'next/link';
+import logo from "../../../public/fav.jpg"
 
 import NavLink from '@/components/header/nav-link';
 import ThemeToggle from '@/components/theme-toggle';
-
+import Image from 'next/image';
 const NAV_ITEMS = [
   { path: 'blog', name: 'BLOG' },
   { path: 'about', name: 'ABOUT' },
@@ -15,14 +16,23 @@ export default function Header() {
   return (
     <nav className="flex flex-col justify-between py-12 md:flex-row ">
       <Link href="/" className="self-start md:self-auto">
-        <h1 className="text-2xl">Subconsiously</h1>
+        <h1 className="text-2xl flex items-center">
+          <Image
+            height={70}
+            width={70}
+            src={logo}
+            alt="logo"
+            style={{ objectFit: 'cover' }}
+          />
+          Subconsiously
+        </h1>
       </Link>
       <div className="my-6 flex space-x-8 self-center md:my-0 md:self-auto">
-        <ul className="flex flex-wrap space-x-8">
+        <ul className="flex flex-wrap space-x-5">
           {NAV_ITEMS.map((item) => (
             <li
               key={item.path}
-              className="text-secondary hover:text-primary whitespace-nowrap py-2 text-lg font-medium transition-all duration-300"
+              className="text-customSecondary hover:text-primary whitespace-nowrap py-2 text-lg font-medium transition-all duration-300"
             >
               <NavLink path={item.path}>{item.name}</NavLink>
             </li>
